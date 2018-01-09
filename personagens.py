cca2e20760e6f1a73f724b33414484973094848e
Anisio Santos <anisio.dev@gmail.com>
2018-01-08 22:37:08 -0300

    funcoes adicionadas

--- personagens.py
+++ personagens.py
@@ -89,3 +89,13 @@ class Ninja_v2 (Personagem):
         print("{}".format(alvo.nome_carta))
         print("Tirando assim: {}".format(alvo.cartas_vida.pop(alvo1)))
         input()
+class Ninja_v3 (Personagem):
+    def __init__(self):
+        super(Ninja_v1, self).__init__()
+    def ataque(self,deck,mao,alvo):
+        self.capacitacao_atual+=1
+        alvo1 = Dano.escolher_carta(self,alvo)
+
+        print("{}".format(alvo.nome_carta))
+        print("Tirando assim: {}".format(alvo.cartas_vida.pop(alvo1)))
+        input()
